f98bffa23c9ed5385a7172d8ea7ac007545703e9
Manuel <carrero.besson@gmail.com>
2025-08-17 21:51:56 -0400

    cambios chart

--- plantium/static/js/chart.js
+++ plantium/static/js/chart.js
@@ -2,8 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const measurements = JSON.parse(
     document.getElementById('measurements-data').textContent
   );
-  console.log(measurements);
-    const labels = Object.values(measurements).map(m => m.date);
+  console.log("hola");
+    let dateStr = Object.values(measurements).map(m => m.date);
+    const date = new Date(dateStr.replace(" ", "T")); 
+
+
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+
+  const labels = `${day}/${month}\n${hours}:${minutes}`;
+  console.log(labels);
+  
   const temps = Object.values(measurements).map(m => m.temp);
   const hums = Object.values(measurements).map(m => m.hum);
 new Chart(document.getElementById('comboChart'), {
