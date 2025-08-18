bbde2f98a6d45eff6839e8325296513657fbe005
Manuel <carrero.besson@gmail.com>
2025-08-17 22:36:23 -0400

    imagenes

--- plantium/static/js/chart.js
+++ plantium/static/js/chart.js
@@ -2,17 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const measurements = JSON.parse(
     document.getElementById('measurements-data').textContent
   );
-    let dateStr = Object.values(measurements).map(m => m.date);
-    const date = new Date(dateStr.replace(" ", "T")); 
-
-
-    const day = date.getDate().toString().padStart(2, "0");
-    const month = (date.getMonth() + 1).toString().padStart(2, "0");
-
-    const hours = date.getHours().toString().padStart(2, "0");
-    const minutes = date.getMinutes().toString().padStart(2, "0");
-
-  const labels = `${day}/${month}\n${hours}:${minutes}`;
+    let labels = Object.values(measurements).map(m => m.date);
   console.log(labels);
   
   const temps = Object.values(measurements).map(m => m.temp);
